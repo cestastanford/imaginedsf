c699d53bf3fcebe8ad67f5447d0be97d19a38bc1
Cody M Leff <codymleff@gmail.com>
2017-05-23 15:44:43 -0700

    Set up basic URL parsing/saving.

--- static-src/components/maps-components/maps.vue
+++ static-src/components/maps-components/maps.vue
@@ -13,19 +13,57 @@
 <!-- Vue component script for the top-level <maps> component. -->
 <script>
 
+
 /*
-*   Assembles and exports the component.
+*   Imports constants and subcomponents.
 */
 
-import { REQUEST_MAPS } from '../maps.js'
+import { REQUEST_MAPS, APPLY_HASH_STATE, GET_HASH_STATE } from '../maps.js'
+
+
+/*
+*   Enables saving state to the URL and listening for URL changes.
+*/
+
+const enableHashRouting = store => {
+
+    //  Applies URL changes to state
+    const applyHashState = () => {
+
+        store.commit(APPLY_HASH_STATE, window.location.hash.slice(1))
+
+    }
+
+    //  Saves state changes to URL
+    const saveHashState = (mutation, state) => {
+
+        window.location.hash = '#' + store.getters[GET_HASH_STATE]
+
+    }
+
+    applyHashState()
+    saveHashState()
+
+    window.addEventListener('popstate', applyHashState)
+    store.subscribe(saveHashState)
+
+}
+
+
+/*
+*   Configures and exports component.
+*/
 
 const Component = {
 
     name: 'maps',
     components: {},
-    mounted() {
-        this.$store.dispatch(REQUEST_MAPS)
-    }
+    async mounted() {
+
+        await this.$store.dispatch(REQUEST_MAPS)
+        enableHashRouting(this.$store)
+
+    },
 
 }
 
--- static-src/components/maps.js
+++ static-src/components/maps.js
@@ -7,74 +7,134 @@ import Vuex from 'vuex'
 
 
 /*
-*   Imports subcomponent.
+*   Imports utilities and subcomponents.
 */
 
 import Maps from './maps-components/maps.vue'
 
 
 /*
-*   Defines state management actions.
+*   Defines state management action constants.
 */
 
 export const REQUEST_MAPS = 'REQUEST_MAPS'
-const SET_PROPOSAL_MAPS = 'SET_PROPOSAL_MAPS'
-const SET_BASEMAPS = 'SET_BASEMAPS'
+export const APPLY_HASH_STATE = 'APPLY_HASH_STATE'
+export const GET_HASH_STATE = 'GET_HASH_STATE'
+const SET_SOURCE_MAPS = 'SET_SOURCE_MAPS'
+
+
+
+/*
+*   Requests and processes maps from WordPress.
+*/
+
+const requestMaps = async ({ commit }) => {
+
+    const REQUEST_URL = '/wp-json/imaginedsf/maps'
+    const response = await fetch(REQUEST_URL)
+    if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`, response)
+    }
+
+    const responseBody = await response.json()
+    const sourceMaps = {}
+    responseBody.forEach(responseBodyObject => {
+
+        const sourceMap = {
+            id: responseBodyObject.ID,
+            title: responseBodyObject.post_title,
+            ...responseBodyObject.fields,
+        }
+
+        sourceMaps[sourceMap.id] = sourceMap
+
+    })
+
+    commit(SET_SOURCE_MAPS, sourceMaps)
+
+}
+
+
+/*
+*   Encodes/decodes objects for saving in URLs.
+*/
+
+const encodeForURL = obj => {
+
+    const cleaned = { ...obj }
+    for (let key in cleaned) {
+        if (!cleaned[key]) delete cleaned[key]
+    }
+
+    return encodeURI(JSON.stringify(cleaned))
+
+}
+
+const decodeFromURL = str => {
+
+    let parameters = {}
+
+    try {
+        parameters = JSON.parse(decodeURI(str))
+    } catch (e) {
+        console.warn('Hash parameter parse failed: ' + str)
+    }
+
+    return parameters
+
+}
 
 
 /*
 *   Sets up state management and binds the root element.
 */
 
-function initMaps(el) {
+const initRootComponent = (el) => {
 
     Vue.use(Vuex)
     const store = new Vuex.Store({
 
         state: {
 
-            proposalMaps: [],
-            basemaps: [],
+            sourceMaps: {},
+            mapState: {
+
+                opacities: null,
+                featureGroups: null,
+                narrative: null,
+                popup: null,
+                address: null,
+                mapBounds: null,
+
+            }
+
+        },
+
+        getters: {
+
+            [GET_HASH_STATE]: state => encodeForURL(state.mapState),
 
         },
 
         mutations: {
 
-            [SET_PROPOSAL_MAPS]: (state, proposalMaps) => state.proposalMaps = proposalMaps,
-            [SET_BASEMAPS]: (state, basemaps) => state.basemaps = basemaps,
+            [SET_SOURCE_MAPS]: (state, sourceMaps) => state.sourceMaps = sourceMaps,
+            [APPLY_HASH_STATE]: (state, parameterString) => {
+
+                const parameters = decodeFromURL(parameterString)
+                for (let key in state.mapState) {
+                    
+                    if (parameters[key]) state.mapState[key] = parameters[key]
+                
+                }
+
+            },
 
         },
 
         actions: { 
 
-            async [REQUEST_MAPS]({ commit }) {
-
-                const REQUEST_URL = '/wp-json/imaginedsf/maps'
-                const response = await fetch(REQUEST_URL)
-                if (!response.ok) {
-                    throw new Error(`${response.status} ${response.statusText}`, response)
-                }
-
-                const responseBody = await response.json()
-                const proposalMaps = {}
-                const basemaps = {}
-                responseBody.forEach(responseBodyObject => {
-
-                    const map = {
-                        id: responseBodyObject.ID,
-                        title: responseBodyObject.post_title,
-                        ...responseBodyObject.fields,
-                    }
-
-                    if (map.map_type === 'proposal') proposalMaps[map.id] = map
-                    else basemaps[map.id] = map
-
-                })
-
-                commit(SET_PROPOSAL_MAPS, proposalMaps)
-                commit(SET_BASEMAPS, basemaps)
-
-            }
+            [REQUEST_MAPS]: requestMaps,
 
         },
 
@@ -99,6 +159,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const QUERY_STRING = '#maps'
     const el = document.querySelector(QUERY_STRING)
-    if (el) initMaps(el)
+    if (el) initRootComponent(el)
 
 })
