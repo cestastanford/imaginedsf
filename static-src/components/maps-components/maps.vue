3cb5085366a89a77e0eee6b5a33cd8dc5d2a3f9d
Cody M Leff <codymleff@gmail.com>
2017-05-23 12:37:13 -0700

    Set up Vuex for state management.

---
+++ static-src/components/maps-components/maps.vue
@@ -0,0 +1,34 @@
+<!-- Vue component template for the top-level <maps> component. -->
+<template>
+    
+    <div class="container">
+        <div class="columns">
+            <div class="column is-narrow"></div>
+            <div class="column"></div>
+        </div>
+    </div>
+
+</template>
+
+<!-- Vue component script for the top-level <maps> component. -->
+<script>
+
+/*
+*   Assembles and exports the component.
+*/
+
+import { REQUEST_MAPS } from '../maps.js'
+
+const Component = {
+
+    name: 'maps',
+    components: {},
+    mounted() {
+        this.$store.dispatch(REQUEST_MAPS)
+    }
+
+}
+
+export default Component
+
+</script>
--- static-src/components/maps.js
+++ static-src/components/maps.js
@@ -3,13 +3,92 @@
 */
 
 import Vue from 'vue'
+import Vuex from 'vuex'
 
 
 /*
 *   Imports subcomponent.
 */
 
-import Maps from './maps.vue'
+import Maps from './maps-components/maps.vue'
+
+
+/*
+*   Defines state management actions.
+*/
+
+export const REQUEST_MAPS = 'REQUEST_MAPS'
+const SET_PROPOSAL_MAPS = 'SET_PROPOSAL_MAPS'
+const SET_BASEMAPS = 'SET_BASEMAPS'
+
+
+/*
+*   Sets up state management and binds the root element.
+*/
+
+function initMaps(el) {
+
+    Vue.use(Vuex)
+    const store = new Vuex.Store({
+
+        state: {
+
+            proposalMaps: [],
+            basemaps: [],
+
+        },
+
+        mutations: {
+
+            [SET_PROPOSAL_MAPS]: (state, proposalMaps) => state.proposalMaps = proposalMaps,
+            [SET_BASEMAPS]: (state, basemaps) => state.basemaps = basemaps,
+
+        },
+
+        actions: { 
+
+            async [REQUEST_MAPS]({ commit }) {
+
+                const REQUEST_URL = '/wp-json/imaginedsf/maps'
+                const response = await fetch(REQUEST_URL)
+                if (!response.ok) {
+                    throw new Error(`${response.status} ${response.statusText}`, response)
+                }
+
+                const responseBody = await response.json()
+                const proposalMaps = {}
+                const basemaps = {}
+                responseBody.forEach(responseBodyObject => {
+
+                    const map = {
+                        id: responseBodyObject.ID,
+                        title: responseBodyObject.post_title,
+                        ...responseBodyObject.fields,
+                    }
+
+                    if (map.map_type === 'proposal') proposalMaps[map.id] = map
+                    else basemaps[map.id] = map
+
+                })
+
+                commit(SET_PROPOSAL_MAPS, proposalMaps)
+                commit(SET_BASEMAPS, basemaps)
+
+            }
+
+        },
+
+    })
+
+    new Vue({
+        
+        el,
+        store,
+        render: createElement => createElement(Maps),
+
+    })
+
+}
 
 
 /*
@@ -20,15 +99,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const QUERY_STRING = '#maps'
     const el = document.querySelector(QUERY_STRING)
-    if (el) {
-
-        new Vue({
-            
-            el,
-            render: createElement => createElement(Maps),
-
-        })
-
-    }
+    if (el) initMaps(el)
 
 })
