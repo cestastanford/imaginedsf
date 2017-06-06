7783c9c98bd14d908ff94bd9d5aba332f2c43fd4
Cody M Leff <codymleff@gmail.com>
2017-06-05 18:40:47 -0700

    Fixed some layout bugs.

--- imaginedsf-custom-theme/header.php
+++ imaginedsf-custom-theme/header.php
@@ -10,16 +10,19 @@
                 <a class="logo" href="/">
                     <img src="<?php echo get_template_directory_uri() . '/images/logo.svg'; ?>" alt="Imagined San Francisco">
                 </a>
-                <div class="secondary-menu">
-                    <?php get_horizontal_nav_menu( get_nav_menu_items( SECONDARY_MENU ) ); ?>
+                <div class="subflex">
+                    <form class="control has-icons-left search-an-address">
+                        <input class="input is-small" type="text" placeholder="search an address...">
+                        <span class="icon is-small is-left"><i class="fa fa-search"></i></span>
+                    </form>
+                    <div class="secondary-menu">
+                        <?php get_horizontal_nav_menu( get_nav_menu_items( SECONDARY_MENU ) ); ?>
+                    </div>
                 </div>
                 <div class="primary-menu">
                     <?php get_horizontal_nav_menu( get_nav_menu_items( PRIMARY_MENU ) ); ?>
                 </div>
-                <form class="control has-icons-left search-an-address">
-                    <input class="input is-small" type="text" placeholder="search an address...">
-                    <span class="icon is-small is-left"><i class="fa fa-search"></i></span>
-                </form>
+                
                 
             </div>
         </header>
