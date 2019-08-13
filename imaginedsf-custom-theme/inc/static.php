d6b608a669a3b8160c1ec3172be987a7cd35d17d
Krain Chen <krainc@stanford.edu>
2019-08-13 15:22:55 -0700

    Removed all Wordpress templates and created single new template.

--- imaginedsf-custom-theme/inc/static.php
+++ imaginedsf-custom-theme/inc/static.php
@@ -4,25 +4,11 @@
 *   Loads CSS styles and JS scripts for front-end rendering.
 */
 
-function enqueue_static_assets() {
+// function enqueue_static_assets() {
 
-    $styles_path = '/static/styles.css';
-    wp_enqueue_style(
-        'theme_styles',
-        get_template_directory_uri() . $styles_path,
-        array(),
-        filemtime( get_stylesheet_directory() . $styles_path )
-    );
+//     wp_enqueue_style( 'theme_styles', get_template_directory_uri() . '/static/styles.css' );
+//     wp_enqueue_script( 'theme_script', get_template_directory_uri() . '/static/script.js' );
 
-    $script_path = '/static/script.js';
-    wp_enqueue_script(
-        'theme_script',
-        get_template_directory_uri() . $script_path,
-        array(),
-        filemtime( get_stylesheet_directory() . $script_path )
-    );
+// }
 
-
-}
-
-add_action( 'wp_enqueue_scripts', 'enqueue_static_assets' );
+// add_action( 'wp_enqueue_scripts', 'enqueue_static_assets' );
