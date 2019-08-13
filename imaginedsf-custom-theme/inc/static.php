150691d18ea8cbd75c3288b95686bcbbf5cb1fd1
Krain Chen <krainc@stanford.edu>
2019-08-13 15:47:23 -0700

    Removed and re-created front-end app for dynamic content (31).

--- imaginedsf-custom-theme/inc/static.php
+++ imaginedsf-custom-theme/inc/static.php
@@ -4,11 +4,11 @@
 *   Loads CSS styles and JS scripts for front-end rendering.
 */
 
-// function enqueue_static_assets() {
+function enqueue_static_assets() {
     
-//     wp_enqueue_style( 'theme_styles', get_template_directory_uri() . '/static/styles.css' );
-//     wp_enqueue_script( 'theme_script', get_template_directory_uri() . '/static/script.js' );
+    wp_enqueue_style( 'theme_styles', get_template_directory_uri() . '/static/styles.css' );
+    wp_enqueue_script( 'theme_script', get_template_directory_uri() . '/static/script.js' );
 
-// }
+}
 
-// add_action( 'wp_enqueue_scripts', 'enqueue_static_assets' );
+add_action( 'wp_enqueue_scripts', 'enqueue_static_assets' );
