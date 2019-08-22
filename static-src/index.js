c4b1d308fcf123b70aa6084c128f4b5a66469162
Krain Chen <krainc@stanford.edu>
2019-08-22 15:52:24 -0700

    Styles updates.

--- static-src/index.js
+++ static-src/index.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
 
 const MyComponent = () => <div>This is dynamic content from React!</div>;
 
 window.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <MyComponent />,
-    document.getElementById('react')
+    document.getElementById('react'),
   );
-})
+});
