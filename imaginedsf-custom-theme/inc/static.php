65a75e195f88ed196076b1ead94424cbd1ea5b32
Krain Chen <krainc@stanford.edu>
2019-08-22 16:15:10 -0700

    Updated style settings.

--- imaginedsf-custom-theme/inc/static.php
+++ imaginedsf-custom-theme/inc/static.php
@@ -6,8 +6,22 @@
 
 function enqueue_static_assets() {
 
-    wp_enqueue_style( 'theme_styles', get_template_directory_uri() . '/static/styles.css' );
-    wp_enqueue_script( 'theme_script', get_template_directory_uri() . '/static/script.js' );
+    // $styles_path = '/static/styles.css';
+    // wp_enqueue_style(
+    //     'theme_styles',
+    //     get_template_directory_uri() . $styles_path,
+    //     array(),
+    //     filemtime( get_stylesheet_directory() . $styles_path )
+    // );
+
+    $script_path = '/static/script.js';
+    wp_enqueue_script(
+        'theme_script',
+        get_template_directory_uri() . $script_path,
+        array(),
+        filemtime( get_stylesheet_directory() . $script_path )
+    );
+
 
 }
 
