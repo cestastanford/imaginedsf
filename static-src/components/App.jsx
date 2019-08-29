a4cdb4ee901891fd8958e7f7a8e01e9ee1a442d1
Cody M Leff <mail@codymleff.com>
2019-08-29 12:28:32 -0700

    Start of integrating Redux.

--- static-src/components/App.jsx
+++ static-src/components/App.jsx
@@ -3,6 +3,16 @@
 */
 
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+
+/*
+* Imports Redux action creators.
+*/
+
+import * as actions from '../state/actions';
+
 
 /*
 *   Imports components.
@@ -13,18 +23,43 @@ import LeafletMap from './LeafletMap';
 import MapControls from './MapControls';
 import Panel from './Panel';
 
-function App() {
-  return (
-    <div>
-    App Component
-      <Header />
+class App extends React.Component {
+  componentDidMount() {
+    const { mapsRequested, mapsReceived } = this.props;
+    mapsRequested();
+    setTimeout(() => {
+      mapsReceived(
+        { 1: 'sample map' }, // maps
+        ['sample proposal range'], // proposalRanges
+        [1], // basemaps
+      );
+    }, 5000);
+  }
+
+  render() {
+    return (
       <div>
-        <LeafletMap />
-        <Panel />
-        <MapControls />
+      App Component
+        <Header />
+        <div>
+          <LeafletMap />
+          <Panel />
+          <MapControls />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
+App.propTypes = {
+  mapsRequested: PropTypes.func.isRequired,
+  mapsReceived: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = null;
+const mapDispatchToProps = {
+  mapsRequested: actions.mapsRequested,
+  mapsReceived: actions.mapsReceived,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- static-src/index.jsx
+++ static-src/index.jsx
@@ -1,20 +1,44 @@
 /*
-*   Imports libraries.
+* Imports styles.
+*/
+
+import './styles.css';
+
+
+/*
+*  Imports libraries.
 */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 
 /*
-*   Imports utilities and subcomponents.
+*  Imports utilities and subcomponents.
 */
 
 import App from './components/App';
+import rootReducer from './state/reducers';
+
+
+/*
+* Create Redux store, attach to app, mount root component.
+*/
+
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+/* eslint-enable */
 
 window.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
     document.getElementById('react'),
   );
 });
