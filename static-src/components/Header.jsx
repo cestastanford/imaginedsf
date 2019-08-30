0244abab347e9397be9a4246b2dd940fd183f8c5
Krain Chen <krainc@stanford.edu>
2019-08-29 17:45:32 -0700

    Added redux: incomplete.

--- static-src/components/Header.jsx
+++ static-src/components/Header.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+/*
+*   Import libraries.
+*/
 
-function Header() {
-  return (
-    <div>Header Component </div>
-  );
-}
+import React from 'react';
+import { Link, Router } from '@reach/router';
+
+/*
+*   Imports components.
+*/
+
+import Bibliography from './Bibliography';
+import Credits from './Credits';
+import Feedback from './Feedback';
+
+const Header = () => (
+  <>
+    <div>
+      <Link to="/bibliography">Bibliography</Link>
+      {' '}
+      <Link to="/credits">Credits</Link>
+      {' '}
+      <Link to="/feedback">Feedback</Link>
+    </div>
+    <Router>
+      <Bibliography path="/bibliography" />
+      <Credits path="/credits" />
+      <Feedback path="/feedback" />
+    </Router>
+  </>
+);
 
 export default Header;
