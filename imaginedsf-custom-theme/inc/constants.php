669f3d84b398059c277e434724f4b8acf2c9fcbf
Cody M Leff <mail@codymleff.com>
2019-09-05 16:48:31 -0700

    Updated README; misc fixes.

--- imaginedsf-custom-theme/inc/constants.php
+++ imaginedsf-custom-theme/inc/constants.php
@@ -7,10 +7,3 @@
 define( 'MAP_POST_TYPE', 'isf_map' );
 define( 'MAP_GROUP_POST_TYPE', 'isf_map_group' );
 define( 'NARRATIVE_POST_TYPE', 'isf_narrative' );
-
-
-/*
-*   Defines menu slugs.
-*/
-
-define( 'PRIMARY_MENU', 'primary' );
