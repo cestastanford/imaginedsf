175cb845b535081d9115353bd01e08d81e09543f
Cody M Leff <mail@codymleff.com>
2019-09-06 10:36:45 -0700

    Back-end updates, including REST API (closes #42).  - Updated PHP to reflect WPCS.  - Added REST endpoints for getting content from Options pages.  - Added ACF fields to standard API responses for WP objects.

--- imaginedsf-custom-theme/inc/constants.php
+++ imaginedsf-custom-theme/inc/constants.php
@@ -1,9 +1,44 @@
 <?php
+/**
+ * Defines constants for the rest of the theme.
+ *
+ * @package Imagined San Francisco Custom Theme
+ */
 
-/*
-*   Defines custom post type constants.
-*/
+/**
+ * Defines custom post type constants.
+ */
 
 define( 'MAP_POST_TYPE', 'isf_map' );
 define( 'MAP_GROUP_POST_TYPE', 'isf_map_group' );
 define( 'NARRATIVE_POST_TYPE', 'isf_narrative' );
+
+
+/**
+ * Defines ACF Options page post ID constants for retrieving content.
+ */
+define( 'CONTENT_AREAS_OPTIONS', 'content-areas' );
+define( 'PROPOSAL_RANGES_OPTIONS', 'proposal-ranges' );
+define( 'BASEMAPS_OPTIONS', 'basemaps' );
+
+
+/*
+ * Defines content area constants.  These should align with the
+ * content areas the front-end application will be requesting (see
+ * `static-src/constants.js`) and the content areas defined in
+ * the WP Admin's Pages > Content Areas ACF Options page.
+ */
+
+define( 'INTRODUCTION_CONTENT_AREA', 'introduction' );
+define( 'BIBLIOGRAPHY_CONTENT_AREA', 'bibliography' );
+define( 'CREDITS_CONTENT_AREA', 'credits' );
+define( 'FEEDBACK_CONTENT_AREA', 'feedback' );
+define(
+	'CONTENT_AREAS',
+	array(
+		INTRODUCTION_CONTENT_AREA,
+		BIBLIOGRAPHY_CONTENT_AREA,
+		CREDITS_CONTENT_AREA,
+		FEEDBACK_CONTENT_AREA,
+	)
+);
--- imaginedsf-custom-theme/inc/static.php
+++ imaginedsf-custom-theme/inc/static.php
@@ -1,19 +1,24 @@
 <?php
+/**
+ * Loads Webpack bundle for front-end app.
+ *
+ * @package Imagined San Francisco Custom Theme
+ */
 
-/*
-*   Loads CSS styles and JS scripts for front-end rendering.
-*/
+/**
+ * Enqueues Webpack bundle.
+ */
+function isf_enqueue_static_assets() {
 
-function enqueue_static_assets() {
-
-    $script_path = '/static/script.js';
-    wp_enqueue_script(
-        'theme_script',
-        get_template_directory_uri() . $script_path,
-        array(),
-        filemtime( get_stylesheet_directory() . $script_path )
-    );
+	$script_path = '/static/script.js';
+	wp_enqueue_script(
+		'theme_script',
+		get_template_directory_uri() . $script_path,
+		array(),
+		filemtime( get_stylesheet_directory() . $script_path ),
+		true
+	);
 
 }
 
-add_action( 'wp_enqueue_scripts', 'enqueue_static_assets' );
+add_action( 'wp_enqueue_scripts', 'isf_enqueue_static_assets' );
