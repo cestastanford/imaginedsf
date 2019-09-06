8a20860b1675b00f075e0338875e47756c76283e
Cody M Leff <mail@codymleff.com>
2019-09-06 10:36:45 -0700

    Back-end updates, including REST API (closes #42).  - Updated PHP to reflect WPCS.  - Added REST endpoints for getting content from Options pages.  - Added ACF fields to standard API responses for WP objects.

--- imaginedsf-custom-theme/functions.php
+++ imaginedsf-custom-theme/functions.php
@@ -1,28 +1,40 @@
 <?php
+/**
+ * Entrypoint for theme initialization code.
+ *
+ * @package Imagined San Francisco Custom Theme
+ */
 
-/*
-*   Defines constants.
-*/
+/**
+ * Defines constants.
+ */
 
 require_once 'inc/constants.php';
 
 
-/*
-*   Updates post types.
-*/
+/**
+ * Updates post types.
+ */
 
 require_once 'inc/custom-post-types.php';
 
 
-/*
-*   Adds ACF Options pages.
-*/
+/**
+ * Adds ACF Options pages.
+ */
 
 require_once 'inc/options.php';
 
 
-/*
-*   Loads site styles and scripts.
-*/
+/**
+ * Loads site styles and scripts.
+ */
 
 require_once 'inc/static.php';
+
+
+/**
+ * Adds and describes REST API endpoints.
+ */
+
+require_once 'inc/rest.php';
--- imaginedsf-custom-theme/inc/options.php
+++ imaginedsf-custom-theme/inc/options.php
@@ -1,28 +1,45 @@
 <?php
+/**
+ * Adds three ACF options pages.  Proposal Ranges and Basemaps
+ * are added under the Maps menu item and Content Areas are added
+ * under Pages.
+ *
+ * @package Imagined San Francisco Custom Theme
+ */
 
-/*
-*   Adds three ACF options pages.  Proposal Ranges and Basemaps
-*   are added under the Maps menu item and Content Areas are added
-*   under Pages.
-*/
+if ( function_exists( 'acf_add_options_page' ) ) {
 
-if( function_exists('acf_add_options_page') ) {
+	acf_add_options_page(
+		array(
+			'page_title'      => 'Content Areas',
+			'icon_url'        => 'dashicons-analytics',
+			'position'        => '20.1',
+			'update_button'   => 'Update Content Areas',
+			'updated_message' => 'Content areas updated.',
+			'post_id'         => CONTENT_AREAS_OPTIONS,
+		)
+	);
 
-    acf_add_options_page(array(
-        'page_title' => 'Proposal Maps',
-        'icon_url' => 'dashicons-menu-alt',
-        'position' => '27.1',
-    ));
+	acf_add_options_page(
+		array(
+			'page_title'      => 'Proposal Ranges',
+			'icon_url'        => 'dashicons-menu-alt',
+			'position'        => '27.1',
+			'update_button'   => 'Update Proposal Ranges',
+			'updated_message' => 'Proposal ranges updated.',
+			'post_id'         => PROPOSAL_RANGES_OPTIONS,
+		)
+	);
 
-    acf_add_options_page(array(
-        'page_title' => 'Basemaps',
-        'icon_url' => 'dashicons-images-alt',
-        'position' => '27.2',
-   ));
-
-    acf_add_options_page(array(
-        'page_title' => 'Content Areas',
-        'parent_slug' => 'edit.php?post_type=page',
-    ));
+	acf_add_options_page(
+		array(
+			'page_title'      => 'Basemaps',
+			'icon_url'        => 'dashicons-images-alt',
+			'position'        => '27.2',
+			'update_button'   => 'Update Basemaps',
+			'updated_message' => 'Basemaps updated.',
+			'post_id'         => BASEMAPS_OPTIONS,
+		)
+	);
 
 }
