cf23f1494cd5b65eac93065a505b48431c2e43ce
Cody M Leff <mail@codymleff.com>
2019-09-18 19:35:40 -0700

    Styled Header component.

--- static-src/components/Header.jsx
+++ static-src/components/Header.jsx
@@ -7,12 +7,14 @@ import logoImg from '../img/logo.svg';
 export default function Header() {
   return (
     <StyledHeader>
-      <StyledLogo src={logoImg} alt="Imagined San Francisco" />
-      <StyledLinks>
+      <StyledLink to="/">
+        <StyledLogo src={logoImg} alt="Imagined San Francisco" />
+      </StyledLink>
+      <NavLinks>
         <StyledLink to="/bibliography">Bibliography</StyledLink>
         <StyledLink to="/credits">Credits</StyledLink>
         <StyledLink to="/feedback">Feedback</StyledLink>
-      </StyledLinks>
+      </NavLinks>
     </StyledHeader>
   );
 }
@@ -21,14 +23,30 @@ const StyledHeader = styled.div`
   display: flex;
   align-items: flex-end;
   justify-content: space-between;
-  height: 3em;
+  padding: 1em 1em 0.5em 1em;
+  box-shadow: 0 0 10px #ddd;
 `;
 
-const StyledLogo = styled.img``;
-
-const StyledLinks = styled.div``;
+const StyledLogo = styled.img`
+  height: 4em;
+`;
 
 const StyledLink = styled(Link)`
-  margin: 1em;
-  font-weight: lighter;
+  color: inherit;
+  text-decoration: none;
+  transition: opacity ${({ theme }) => theme.transitionDurations.linkHover};
+
+  &:hover {
+    opacity: ${({ theme }) => theme.opacities.linkHover};
+  }
+`;
+
+const NavLinks = styled.div`
+  margin-bottom: 0.85em;
+  color: ${({ theme }) => theme.colors.darkGrey};
+  text-transform: lowercase;
+
+  ${StyledLink} {
+    margin: 0 1em;
+  }
 `;
