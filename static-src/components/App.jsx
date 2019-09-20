efd3e60dc6b1dc8cff060f7152633dae200877af
Cody M Leff <mail@codymleff.com>
2019-09-19 22:33:59 -0700

    WIP on Leaflet map.

--- static-src/components/App.jsx
+++ static-src/components/App.jsx
@@ -2,15 +2,26 @@
 * Imports libraries.
 */
 
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
+
+
+/*
+* Imports Redux action creators.
+*/
+
+import {
+  fetchContent,
+  setDefaultMapState,
+  setMapStateFromHash,
+} from '../state/actions';
 
 
 /*
 * Imports components.
 */
 
-import ContentLoader from './ContentLoader';
 import Header from './Header';
 import LeafletMap from './LeafletMap';
 import VisibleMapArea from './VisibleMapArea';
@@ -19,13 +30,70 @@ import Modals from './Modals';
 
 
 /*
-* Defines top-level App component hierarchy.
+* Encapsulates the logic that synchronizes the map state and the
+* URL hash.
+*/
+
+function useHashMapState(loading) {
+  const dispatch = useDispatch();
+  const mapState = useSelector((state) => state.mapState);
+
+  //  Hash change handler
+  const handleHashChange = useCallback(() => {
+    dispatch(setMapStateFromHash(window.location.hash));
+  }, [dispatch]);
+
+  //  Adds hash change listener
+  useEffect(() => {
+    if (!loading) {
+      window.addEventListener('hashchange', handleHashChange);
+      return () => {
+        window.removeEventListener('hashchange', handleHashChange);
+      };
+    }
+
+    return undefined;
+  }, [loading, handleHashChange]);
+
+  //  Removes listener and updates hash on map state change
+  useEffect(() => {
+    if (!loading) {
+      window.removeEventListener('hashchange', handleHashChange);
+      window.location.hash = encodeURIComponent(JSON.stringify({ mapState }));
+      window.addEventListener('hashchange', handleHashChange);
+    }
+  }, [loading, mapState, handleHashChange]);
+}
+
+
+/*
+* Defines top-level App component, which contains all other app components
+* and starts initial data loading and state initialization.
 */
 
 export default function App() {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+
+  //  Downloads initial content and sets initial state
+  useEffect(() => {
+    const initApp = async () => {
+      await dispatch(fetchContent());
+      dispatch(setDefaultMapState());
+      dispatch(setMapStateFromHash(window.location.hash));
+      setLoading(false);
+    };
+
+    initApp();
+  }, [dispatch]);
+
+  //  Synchronizes mapState with URL hash
+  useHashMapState(loading);
+
   return (
     <StyledApp>
-      <ContentLoader>
+      <LoadingMessage visible={loading}>Loading...</LoadingMessage>
+      { loading ? null : (
         <StyledMainContainer>
           <Header />
           <StyledBody>
@@ -35,7 +103,7 @@ export default function App() {
             <Modals />
           </StyledBody>
         </StyledMainContainer>
-      </ContentLoader>
+      )}
     </StyledApp>
   );
 }
@@ -44,6 +112,24 @@ export default function App() {
 /*
 * Defines styled components used in app.
 */
+
+const LoadingMessage = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: ${({ theme }) => theme.zIndices.LoadingMessage};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  font-size: 2em;
+  font-weight: lighter;
+  visibility: ${({ visible }) => (visible ? 'visible' : 'hidden')};
+  background-color: #fff;
+  opacity: ${({ visible }) => (visible ? 1 : 0)};
+  transition: opacity 0.5s, visibility 0s 0.5s;
+`;
 
 const StyledApp = styled.div`
   width: 100%;
