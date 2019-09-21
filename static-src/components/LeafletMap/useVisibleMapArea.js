65cb92bf0996f5159be2faec99cca13bdff529b2
Cody M Leff <mail@codymleff.com>
2019-09-20 18:56:27 -0700

    Almost done with LeafletMap component

--- static-src/components/LeafletMap/useVisibleMapArea.js
+++ static-src/components/LeafletMap/useVisibleMapArea.js
@@ -1,9 +1,36 @@
+/*
+* Imports.
+*/
+
+import { useRef, useEffect } from 'react';
+
+
+/*
+* Exposes a proxy object that represents the visible area of the Leaflet
+* map and allows certain method calls on it, forwarding those actions
+* to the Leaflet map to give the desired response or effect.
+*/
+
 export default function useVisibleMapArea(leafletMap, visibleMapAreaRef) {
-  return {
-    getBounds: () => {},
-    getCenter: () => {},
-    getZoom: () => {},
-    fitBounds: () => {},
-    setView: () => {},
-  };
+  const visibleMapAreaProxy = useRef();
+
+  useEffect(() => {
+    const { current: map } = leafletMap;
+    if (map) {
+      visibleMapAreaProxy.current = {
+        getBounds: map.getBounds.bind(map),
+        getCenter: map.getCenter.bind(map),
+        getZoom: map.getZoom.bind(map),
+        fitBounds: map.fitBounds.bind(map),
+        setView: map.setView.bind(map),
+        hasLayer: map.hasLayer.bind(map),
+        addLayer: map.addLayer.bind(map),
+        removeLayer: map.removeLayer.bind(map),
+        on: map.on.bind(map),
+        off: map.off.bind(map),
+      };
+    }
+  }, [leafletMap]);
+
+  return visibleMapAreaProxy;
 }
