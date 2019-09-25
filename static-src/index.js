6dee93f2636513e4737d40edf94a6b7b6d4e5f58
Cody M Leff <mail@codymleff.com>
2019-09-25 13:25:20 -0700

    Safari fix for Redux dev tools.

--- static-src/index.js
+++ static-src/index.js
@@ -36,7 +36,9 @@ const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    window.navigator.userAgent.includes('Chrome')
+      ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      : compose,
   ),
 );
 /* eslint-enable */
