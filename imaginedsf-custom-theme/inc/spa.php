ab211ff56ac2eb9b2bd59763e71245469a250a1e
Cody M Leff <mail@codymleff.com>
2019-11-06 10:55:46 -0800

    Added PHP redirection to HTTPS if the site URL is set to HTTPS (fixes #58).

--- imaginedsf-custom-theme/inc/spa.php
+++ imaginedsf-custom-theme/inc/spa.php
@@ -6,6 +6,24 @@
  * @package Imagined San Francisco Custom Theme
  */
 
+// Redirects to HTTPS.
+add_filter(
+	'init',
+	function() {
+		$site_url_scheme = wp_parse_url( get_option( 'siteurl' ) )['scheme'];
+		if ( 'https' === $site_url_scheme && ! is_ssl() ) {
+			if ( isset( $_SERVER['HTTP_HOST'] ) && isset( $_SERVER['REQUEST_URI'] ) ) {
+				// phpcs:ignore WordPress.Security.ValidatedSanitizedInput
+				$location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+				// phpcs:ignore WordPress.Security.SafeRedirect
+				wp_redirect( $location, 301 );
+				exit;
+			}
+		}
+	}
+);
+
+
 // Disables returning 404s and rewriting URL to match existing WP
 // objects for paths not matching WordPress paths.
 remove_filter( 'remplate_redirect', 'redirect_canonical' );
