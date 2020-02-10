7a8bf99d7dddf20f729536d35b9a0bf9940051e8
Cody M Leff <mail@codymleff.com>
2020-02-09 19:50:18 -0800

    Added FB/Twitter links (#52).

--- static-src/components/Modals/ShareModal.js
+++ static-src/components/Modals/ShareModal.js
@@ -1,16 +1,55 @@
 import React, { useContext } from 'react';
+import styled from 'styled-components';
 
 import Modal from './Modal';
 import PreviousLocationContext from './PreviousLocationContext';
+import facebookIcon from '../../img/facebook-icon.png';
+import twitterIcon from '../../img/twitter-icon.png';
 
 
 export default function ShareModal() {
   const previousLocation = useContext(PreviousLocationContext);
-  const previousUrl = `${window.location.origin}${previousLocation.pathname}${previousLocation.hash}`;
+
+  const shareUrl = `${window.location.origin}${previousLocation.pathname}${previousLocation.hash}`;
+  const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(shareUrl)}`;
+  const twitterShareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(shareUrl)}`;
 
   return (
     <Modal title="Share current view" isSideModal>
-      <a href={previousUrl}>{previousUrl}</a>
+      <StyledSocialLinks>
+        <StyledSocialLink href={facebookShareUrl} target="_share">
+          <img src={facebookIcon} alt="Facebook" />
+          Facebook
+        </StyledSocialLink>
+        <StyledSocialLink href={twitterShareUrl} target="_share">
+          <img src={twitterIcon} alt="Twitter" />
+          Twitter
+        </StyledSocialLink>
+      </StyledSocialLinks>
     </Modal>
   );
 }
+
+const StyledSocialLinks = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin: 1.5em;
+`;
+
+const StyledSocialLink = styled.a`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 0.75em;
+  font-weight: bold;
+  color: initial;
+  transition: opacity ${({ theme }) => theme.transitionDurations.linkHover};
+
+  &:hover {
+    opacity: ${({ theme }) => theme.opacities.linkHover};
+  }
+
+  img {
+    width: 4em;
+  }
+`;
